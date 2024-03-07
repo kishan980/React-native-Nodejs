0d3fc59d2e45bce5ba27aeb4fd4f93dd0169f47a
kishan980 <kishany980@gmail.com>
2024-03-07 23:14:06 +0530

    setup database connection and morgan and env file update

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require("express")
 const app =express();
-require("dotenv/config")
-
+const morgan =require("morgan")
+const mongoose =require("mongoose")
 
 //middleware body parser
 app.use(express.json())
+app.use(morgan('tiny'))
+//env file fetch if require line all file  direct use
+require("dotenv/config")
+const api = process.env.API_URL;
+app.get(`${api}/products`, (req,res)=>{
+  const products ={
+    id:1,
+    name:"kishan yadav",
+    image:"kishan.jpg"
+  }
+  res.send(products)
+})
+
+app.post(`${api}/products`, (req,res) =>{
+  const newProduct =req.body;
+  console.log(newProduct);
+  res.send(newProduct)
+})
 app.get("/", (req,res)=>{
   res.send("Hello Api ")
+})
+
+mongoose.connect(process.env.DATA_BASE_URL).then((conn)=>{
+  console.log("Database connection Done...")
+}).catch((err)=>{
+  console.log(err)
 })
 
 app.listen(process.env.PORT, ()=>{
